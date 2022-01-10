16b7bbd3fe134300889a35fab7273bcc5739f939
Marvin Wiemers <92319679+Mastermuffin92@users.noreply.github.com>
2022-01-10 17:18:21 +0100

    added images and startet to work on slideshow

--- Desktop/DeveloperAkademie/marvinwiemers.de/src/app/app.component.ts
+++ Desktop/DeveloperAkademie/marvinwiemers.de/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'marvinwiemers.de';
+export class AppComponent implements OnInit {
+  images = ['Hintergrund-Laptop.jpg','Hintergrund-Laptop2.jpg','Hintergrund-Laptop3.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+  updateImage() {
+    setInterval(() =>{
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() =>{
+        this.showImage=true;
+      },100);
+    },8000);
+  }
 }
